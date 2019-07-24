870c3d05fccfe0efe63862e8bfd9ea18c043a2d8
nedenis <53235724+nedenis@users.noreply.github.com>
2019-07-24 15:08:48 +0300

    First try

---
+++ ForDown.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Воде получается(нет)</title>
+</head>
+<body>
+	<h1><font color="green">Лох</font></h1>
+	<a href="https://vk.com/monogatariiii">Лох</a> ванючка
+
+
+</body>
+</html>
